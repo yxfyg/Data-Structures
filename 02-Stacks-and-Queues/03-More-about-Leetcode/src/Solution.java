6315682ee12c4f1bd43a6292ab076e8c2d3cfdb5
yxfyg <1513032120@stmail.ntu.edu.cn>
2020-10-27 23:33:00 +0800

    使用自定义的栈解决括号匹配问题

---
+++ 02-Stacks-and-Queues/03-More-about-Leetcode/src/Solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+
+    public boolean isValid(String s){
+
+        ArrayStack<Character> stack = new ArrayStack<>();
+        for(int i = 0 ; i < s.length() ; i ++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+            }
+            else {
+                if(stack.isEmpty()){
+                    return false;
+                }
+
+                char topChar = stack.pop();
+                if(c == ')' && topChar != '('){
+                    return false;
+                }
+                if(c == ']' && topChar != '['){
+                    return false;
+                }
+                if(c == '}' && topChar != '{'){
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
