dff1f81ed7cc75cb61e51e7c1e4b62b3c55095c2
yxfyg <1513032120@stmail.ntu.edu.cn>
2020-12-30 21:02:54 +0800

    借助栈结构以非递归的方式前序遍历二分搜索树

---
+++ 05-Binary-Search-Tree/07-Non-Recursion-Preorder-Traverse-in-BST/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 6, 7, 8, 2, 7, 4, 9, 1, 3};
+
+        for(int num : nums){
+            bst.add(num);
+        }
+
+        bst.preOrder();
+        System.out.println();
+        bst.preOrderNR();
+
+//        bst.inOrder();
+//        System.out.println();
+//
+//        bst.postOrder();
+//        System.out.println();
+    }
+}
+
+    //思考题：重写二分搜索树的toString方法，输出如下形式的字符串
+      /////////////////
+      //      5      //
+      //    /   \    //
+      //   3    6    //
+      //  / \    \   //
+      // 2  4     8  //
+      /////////////////
